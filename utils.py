d07e2c68dfebda95ab494c526bc92ba5f06e9399
Jakub Wójcik <272610@student.pwr.edu.pl>
2026-05-07 12:25:52 +0200

    Konfiguracja CI/CD i pliki i utils bo się nie zapisał i poprawa komentarzy

--- utils.py
+++ utils.py
@@ -1,14 +1,19 @@
 # przykładowe funkcje utils.py
+"""Moduł utils.py zawiera podstawowe funkcje matematyczne: dodawanie, odejmowanie, mnożenie i dzielenie."""
 def add(a: int, b: int) -> int:
+    """"Funkcja dodająca dwie liczby całkowite."""
     return a + b
 
 def subtract(a: int, b: int) -> int:
+    """Funkcja odejmująca dwie liczby całkowite."""
     return a - b
 
 def multiply(a: int, b: int) -> int:
+    """Funkcja mnożąca dwie liczby całkowite."""
     return a * b
 
 def divide(a: int, b: int) -> float:
+    """Funkcja dzieląca dwie liczby całkowite."""
     #if b == 0:
     #    raise ValueError("Cannot divide by zero")
     return a / b
