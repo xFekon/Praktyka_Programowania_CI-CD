4175b8670c41ab5e7d82c412a33472261465b0a5
Jakub Wójcik <272610@student.pwr.edu.pl>
2026-05-07 12:21:03 +0200

    Konfiguracja CI/CD i pliki i utils bo się nie zapisał

---
+++ utils.py
@@ -0,0 +1,14 @@
+# przykładowe funkcje utils.py
+def add(a: int, b: int) -> int:
+    return a + b
+
+def subtract(a: int, b: int) -> int:
+    return a - b
+
+def multiply(a: int, b: int) -> int:
+    return a * b
+
+def divide(a: int, b: int) -> float:
+    #if b == 0:
+    #    raise ValueError("Cannot divide by zero")
+    return a / b
